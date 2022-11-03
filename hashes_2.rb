8107129cf5a38974837d510b07c8a2611d2a22a7
Matt Enyeart <menyeart1@gmail.com>
2022-11-03 13:26:35 -0600

    Add solutions to the hashes 2 exercise

--- hashes_2.rb
+++ hashes_2.rb
@@ -11,7 +11,9 @@ states = {
 #
 #
 #
-
+states.each do |state, abbreviation|
+  puts "The abbreviation for #{state} is #{abbreviation}."
+end
 
 # ======================================
 # 2- Using the following Hash
@@ -27,7 +29,9 @@ birthdays = {
 #
 #
 #
-
+birthdays.each do |name, birthday|
+  puts "Did you know that #{name} was born on #{birthday}?"
+end
 
 # ======================================
 # 3- Using the following Hash
@@ -43,3 +47,8 @@ login_statuses = {
 #
 #
 #
+login_statuses.each do |name, available|
+  if available
+    puts name
+  end
+end
